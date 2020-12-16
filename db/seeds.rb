f8ba4e3af7265baf6ea0c8839637845a534d688b
chsohn15 <chsohn15@gmail.com>
2020-12-16 17:17:29 -0500

    remove faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
+
 Store.destroy_all
 Dessert.destroy_all
 
