5bb3662ad5a7059a9bbc7fbb6b303339bd422664
chsohn15 <chsohn15@gmail.com>
2020-12-16 18:56:21 -0500

    migrations

--- db/migrate/20201216235552_add_column_to_desserts.rb
+++ db/migrate/20201216235552_add_column_to_desserts.rb
@@ -1,4 +1,4 @@
-class AddColumnToDessertsTable < ActiveRecord::Migration[6.0]
+class AddColumnToDesserts < ActiveRecord::Migration[6.0]
   def change
     add_column :desserts, :store_id, :integer
   end
